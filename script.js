77371924f5a044df68eebdbfc2317c8130988de8
joaokalina010-ctrl <joaokalina010@gmail.com>
2026-07-03 01:35:06 -0300

    Criar script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function show(id){
+  let pages = document.querySelectorAll(".page");
+  pages.forEach(p => p.classList.remove("active"));
+
+  document.getElementById(id).classList.add("active");
+}
